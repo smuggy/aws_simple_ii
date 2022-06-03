842fbe1f2645846265baf9bccf964f240482d390
Marc McKernan <marc.mckernan@gmail.com>
2022-06-03 15:26:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,4 +33,11 @@ locals {
 #    tags = {
 #        Name = "simple-instance-ii"
 #    }
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
 #}

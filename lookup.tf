a53e04033a20f1220e3292cdfc422046db28c6bc
Marc McKernan <marc.mckernan@gmail.com>
2022-06-03 15:15:41 -0500

    Update lookup.tf

--- lookup.tf
+++ lookup.tf
@@ -1,14 +1,14 @@
-data "aws_security_group" "vpc_secgrp" {
-  name = local.secgrp_name
-}
+#data "aws_security_group" "vpc_secgrp" {
+#  name = local.secgrp_name
+#}
 
-data "aws_vpc" "kube_vpc" {
-  tags = {
-    Name = "kube-us-east-2-vpc"
-  }
-}
+#data "aws_vpc" "kube_vpc" {
+#  tags = {
+#    Name = "kube-us-east-2-vpc"
+#  }
+#}
 
-data "aws_subnet" "kube_subnet" {
-  vpc_id            = data.aws_vpc.kube_vpc.id
-  availability_zone = local.availability_zone
-}
+#data "aws_subnet" "kube_subnet" {
+#  vpc_id            = data.aws_vpc.kube_vpc.id
+#  availability_zone = local.availability_zone
+#}
